e33b4286db55a1b9920553a33479cda478ae9930
M Farhan Hashmi <160288691+123Simplegithub@users.noreply.github.com>
2024-05-06 02:48:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var answer = "done";
+if (answer === "done") {
+    console.log("you got it right!");
+}
+else {
+    console.log("Sorry, that's not right");
+}
---
+++ app.ts
@@ -0,0 +1,6 @@
+let answer: string = "done"
+if(answer === "done") {
+    console.log("you got it right!");
+} else {
+    console.log("Sorry, that's not right");
+}
